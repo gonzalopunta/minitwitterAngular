04d72d90d6d0e87f0f2665347bdcafbed9785565
gonzalopunta <gpuntaperez@safareyes.es>
2020-11-30 13:14:45 +0100

    pequeños cambios

--- minitwitterApp/src/app/componets/login/login.component.ts
+++ minitwitterApp/src/app/componets/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Login } from 'src/app/models/login.interface';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
 
   usuario: Login;
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService, private route:ActivatedRoute, private router:Router) { 
     this.usuario = new Login('', '');
   }
 
@@ -19,11 +20,14 @@ export class LoginComponent implements OnInit {
   }
 
   doLogin() {
-    // Llamar a un servicio que mande la petición de login
-    // a la API.
     this.authService.login(this.usuario).subscribe(res => {
       localStorage.setItem('token', res.token);
+      this.router.navigate(["/inicio"]);
     });
+  }
+
+  registrate(){
+    this.router.navigate(["/registrar"]);
   }
 
 }
